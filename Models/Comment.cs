5f9acd913045fd0d6e3725ea2dc2fcfef8d724b9
Ryan McQuerry <ryan.mcquerry@fayette.kyschools.us>
2019-01-09 09:53:41 -0500

    init

--- Models/Comment.cs
+++ Models/Comment.cs
@@ -1,16 +1,21 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace IST_Submission_Form.Models
 {
     public class Comment
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ID { get; set; }
         public int SubmissionID { get; set; }
+        [ForeignKey("SubmissionID")]
+        public virtual Submission Submission { get; set; }
+        [Column(TypeName = "text")]
         public string Body { get; set; }
         public string CreatedBy { get; set; }
         [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:g}")]
         public DateTime CreatedAt { get; set; }
-        
+
     }
 }
